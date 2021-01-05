f9d8c1cac43456fdabe514f05061a9f74dc132d9
Pavel Grinchenko <74345316+pablooeescobaro@users.noreply.github.com>
2021-01-05 13:44:02 +0200

    Add all task)

---
+++ js_5/js-9.js
@@ -0,0 +1,8 @@
+function nineTask() {
+    for (let num = 1; num <= 9; num++){
+        for(let factor = 1; factor <=10; factor++){
+            console.log(`${num} * ${factor} = ${num * factor}`);
+        }
+        console.log(`==============`); 
+    }
+}
