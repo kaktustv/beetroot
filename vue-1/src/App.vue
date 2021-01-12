26bbe5d942a50ffada8e55cf859c297cc8f8e21c
Pasha Grinchenko <kvartalshow@gmail.com>
2021-01-12 10:29:06 +0200

    add vue 1

---
+++ vue-1/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+      <p>Count: {{count}}</p>
+      <button @click="count += 1">plus</button>
+      <button @click="count -= 1">minus</button>
+      <button @click="count = 0">reset</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"Counter",
+    data(){
+        return{
+         count: 0
+        }
+    }
+}
+</script>
+<style lang="scss">
+
+</style>
