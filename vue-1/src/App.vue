e56ee202eccc97244294ff6d5f436c5d87ca8f1e
Pasha Grinchenko <kvartalshow@gmail.com>
2021-01-16 17:59:36 +0200

    0

--- vue-1/src/App.vue
+++ vue-1/src/App.vue
@@ -1,27 +1,42 @@
 <template>
   <div id="app">
     <ol>
-      <li :v-for="todo in todos" :key="todo.text">
+      <li v-for="todo in todos" :key="todo.text">
         {{ todo.text }}
       </li>
     </ol>
-  <p>  werwewer  </p>  
+    <ol>
+      <app-li
+        v-for="item in groceryList"
+        v-bind:todo="item"
+        v-bind:key="item.id"
+      ></app-li>
+      <span :style="color: red;">adfsdf</span>
+    </ol>
+    <p>{{Number(msg.toString().split('').reverse().join(''))}}</p>
   </div>
 </template>
 
 <script>
+import AppLi from './components/AppLi';
+
 export default {
+  name: 'App',
+  components: {
+    AppLi,
+  },
   data() {
     return {
-      todos: [
-        { text: "Изучить JavaScript" },
-        { text: "Изучить Vue" },
-        { text: "Создать что-нибудь классное" },
+      msg:121214,
+      todos: [{ text: 'One' }, { text: 'Two' }, { text: 'Three' }],
+      groceryList: [
+        { id: 0, text: 'Овощи' },
+        { id: 1, text: 'Сыр' },
+        { id: 2, text: 'Что там ещё люди едят?' },
       ],
     };
   },
 };
 </script>
 
-<style>
-</style>
+<style></style>
